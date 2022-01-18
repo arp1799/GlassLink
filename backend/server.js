2362aac5a32b06f81b0f41d2e88a705d3c3ef431
arp1799 <59207098+arp1799@users.noreply.github.com>
2022-01-19 03:04:57 +0530

    Inital Setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`backend server running at ${PORT}`))
